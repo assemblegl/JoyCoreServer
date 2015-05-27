7449546ec37406a994dc2849777608d0d5fb3648
assemblegl <27513608@qq.com>
2015-05-27 14:57:15 +0800

    数据返回拼装完成

--- src/gl/netty/ServerHandler.java
+++ src/gl/netty/ServerHandler.java
@@ -1,6 +1,7 @@
 package gl.netty;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
 import org.apache.log4j.Logger;
@@ -13,8 +14,7 @@ import io.netty.channel.SimpleChannelInboundHandler;
 
 
 public class ServerHandler extends SimpleChannelInboundHandler<String>{	
-	private static Logger logger = Logger.getLogger(ServerHandler.class); 
-	
+	private static Logger logger = Logger.getLogger(ServerHandler.class); 	
 	private Presto presto;
 	
 	public ServerHandler(String Pip,String Pport,String Pusername,String Ppasswd){		
@@ -25,33 +25,16 @@ public class ServerHandler extends SimpleChannelInboundHandler<String>{
     public void channelRead0(ChannelHandlerContext ctx, String msg) throws Exception {
     	String res = "";
 		try{
-			System.out.println("msg:"+msg+".");	    	 	
+			printInfo("sql:"+msg+".");	    	 	
 			ResultSet rs = presto.exec(msg);			
+			res = analyseRs(rs);
 			
-			if(null == rs){
-				try{
-					Thread.sleep(500);
-				}catch(InterruptedException e){
-					res+=e.getMessage();
-				}
-			}
-			
-			if(null != rs){	
-				try {
-					while (rs.next()) {						
-						res+=rs.getString(1)+","+rs.getString(2)+","+rs.getString(3)+"\n";
-						//System.out.println(rs.getString(1)+","+rs.getString(2)+","+rs.getString(3));
-						}
-					rs.close();
-				} catch (SQLException e) {			
-					e.printStackTrace();
-					res+=e.getMessage();
-				} 
-			}else{				
-				res+="error! ResultSet is null";
-			}			
-			
+		}catch(SQLException e){
+			res += e.getMessage();			
+		}catch(InterruptedException e){
+			res+=e.getMessage();
 		}finally{
+			printInfo(res);
 			final ChannelFuture f = ctx.writeAndFlush(res);
 	        f.addListener(new ChannelFutureListener() {
 	            @Override
@@ -64,6 +47,52 @@ public class ServerHandler extends SimpleChannelInboundHandler<String>{
                 
     }
 
+    private String analyseRs(ResultSet rs) throws SQLException,InterruptedException{
+    	if(null == rs) Thread.sleep(500);   	
+    	if(null == rs) return "rs is null";
+    	
+    	ResultSetMetaData rmd = rs.getMetaData();
+    	int columnNum=rmd.getColumnCount();    	
+    	String res="";
+    	
+    	//循环枚举出所有字段名
+		for(int i=0;i<columnNum;i++){			
+			if(i != 0){
+				res+=",";
+			}
+			
+	          //第一个字段名从1开始
+	    	   res += rmd.getColumnName(i+1);	    	   	    	   
+		}
+    	
+		//字段名输出完成换行
+    	res+="\n";
+		
+    	try {
+			while (rs.next()) {				
+			
+				for(int i=0;i<columnNum;i++){			
+					if(i != 0){
+						res+=",";
+					}
+					
+			          //第一个字段名从1开始
+			    	   res += rs.getString(i+1);	    	   	    	   
+				}
+				res+="\n";
+								
+			}
+			
+			rs.close();
+		} catch (SQLException e) {			
+			e.printStackTrace();
+			return e.getMessage();
+		} 
+    	
+    	return res;
+  	
+    }
+    
     @Override
     public void channelInactive(ChannelHandlerContext ctx) throws Exception {        
     }
